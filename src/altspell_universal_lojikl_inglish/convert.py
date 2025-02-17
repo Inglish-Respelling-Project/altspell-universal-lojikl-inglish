24fa2dd564dc1eb8776da5f7582552831252712e
Nicholas Johnson <njohnson@posteo.net>
2025-02-17 00:00:00 +0000

    Update copyright year

--- src/altspell_universal_lojikl_inglish/convert.py
+++ src/altspell_universal_lojikl_inglish/convert.py
@@ -1,6 +1,6 @@
 '''
     Altspell-Universal-Lojikl-Inglish  Universal Lojikl Inglish plugin for altspell.
-    Copyright (C) 2024  Nicholas Johnson
+    Copyright (C) 2024-2025  Nicholas Johnson
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
